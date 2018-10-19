e25e44df4b3e67fb47d7011fefb3643112013847
Cleber Marrara <cleber.marrara.prado@gmail.com>
2018-10-19 10:29:52 -0300

    view/avisos_dash.php

--- view/avisos_dash.php
+++ view/avisos_dash.php
@@ -2,8 +2,8 @@
 	<div class="col-md-12">
 		<div class="row">
 			<div id="pgb_status" class="progress progress-md active">
-				<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="36" aria-valuemin="0" aria-valuemax="100" style="width:100%;">
-					<span class="">Atualização do Domínio [Download da Atualização 10.1A 10] - 100%</span>
+				<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="36" aria-valuemin="0" aria-valuemax="100" style="width:0%;">
+					<span class="">Atualização do Domínio [Download da Atualização 10.1A 11] - 0%</span>
 				</div>
 			</div>
 		</div>
